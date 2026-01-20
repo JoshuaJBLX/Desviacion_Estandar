ce574302aba69f80265a7639b76144356eed0ff2
JoshuaBLX <73940475@continental.edu.pe>
2026-01-19 19:19:27 -0500

    Caso de prueba para un solo elemento

--- tests/test_desviacion_estandar.py
+++ tests/test_desviacion_estandar.py
@@ -1,5 +1,6 @@
 class TestDesviacionEstandar(unittest.TestCase):
-    def test_desviacion_estandar_conjunto_vacio(self):
-        numeros = []
-        with self.assertRaises(ValueError):
-            calcular_desviacion_estandar(numeros)
+    def test_desviacion_estandar_un_elemento(self):
+        numeros = [5]
+        desviacion_esperada = 0.0
+        desviacion_actual = calcular_desviacion_estandar(numeros)
+        self.assertAlmostEqual(desviacion_esperada, desviacion_actual, places=2)
